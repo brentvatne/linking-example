f9218c6bede67e8bcf1afe63492b75d9e8367995
Brent Vatne <brentvatne@gmail.com>
2021-08-10 14:24:51 -0700

    ok

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Linking from 'expo-linking';
 
 export default function App() {
+  const url = Linking.useURL();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Initial / updated URL: {url}</Text>
+      <Text>Deep link URL: {Linking.createURL('example')}</Text>
       <StatusBar style="auto" />
     </View>
   );
